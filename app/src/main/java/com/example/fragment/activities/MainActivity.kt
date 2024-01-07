5f37011508d39872e44ae06ab79897f48ae90e94
josipfrljic94 <josipfrljic77@gmail.com>
2024-01-07 18:36:02 +0100

    chore drawer layout

--- app/src/main/java/com/example/fragment/activities/MainActivity.kt
+++ app/src/main/java/com/example/fragment/activities/MainActivity.kt
@@ -2,14 +2,14 @@ package com.example.fragment.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.fragment.app.Fragment
+import androidx.core.view.GravityCompat
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.fragment.R
 import com.example.fragment.databinding.ActivityMainBinding
 import com.example.fragment.fragments.HomeFragment
 import com.example.fragment.fragments.ListOnlineFragment
-import com.example.fragment.service.MealService
-import com.example.fragment.service.NetworkSource
 import com.google.android.material.navigation.NavigationView
 
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,52 +19,40 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var hf:HomeFragment
     private lateinit var loF:ListOnlineFragment
+
+    private val navController by lazy {
+        Navigation.findNavController(this, R.id.nav_host_fragment)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding=ActivityMainBinding.inflate(layoutInflater)
         val view= binding.root
         setContentView(view)
-        binding.topAppBar.setNavigationOnClickListener(){
-            binding.drawerLayout.open()
-        }
 
-        binding.navView.setNavigationItemSelectedListener { menuItem ->
-            // Handle menu item selected
-            menuItem.isChecked = true
-            binding.drawerLayout.close()
-            true
-        }
-        hf= HomeFragment()
-        loF= ListOnlineFragment()
-        fragmentSupplayer(hf)
+        setSupportActionBar(binding.topAppBar)
         setupDrawerContent(binding.navView)
 
-//        NetworkSource().getMealsData(mapOf("keyOf" to "dc953b44c20c4685b0f7d980b7e90d8b"))
     }
 
     private fun setupDrawerContent(navigationView: NavigationView){
-        navigationView.setNavigationItemSelectedListener{menuItem->
-            selectDrawerItem(menuItem)
-            true
-        }
-    }
-
-    private fun selectDrawerItem(menuItem: MenuItem):Unit {
-        when(menuItem.itemId){
-            R.id.homeFragment->fragmentSupplayer(hf)
-            R.id.listOnlineFragment->fragmentSupplayer(loF)
-        }
+        navigationView.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController,binding.drawerLayout)
     }
 
 
-    private  fun fragmentSupplayer(fragment:Fragment){
-        supportFragmentManager.beginTransaction().apply {
-            replace(binding.flContent.id,fragment)
-            commit()
-            if(!binding.drawerLayout.isActivated){
-                binding.drawerLayout.close()
-            }
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, binding.drawerLayout)
+    }
+
+
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
     }
+
 }
